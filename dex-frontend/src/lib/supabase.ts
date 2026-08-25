99325fa048c24e0939731e125b558d8fc7dd40a3
XieHang <850525344@qq.com>
2026-08-25 09:58:57 +0800

    暂存

--- dex-frontend/src/lib/supabase.ts
+++ dex-frontend/src/lib/supabase.ts
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
+// supabase Tse Hang 使用github登录，databse password xNiPEQJbRLtNci9l
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
